4a6c9aa099938c57eea436261d0cd8ae414e63b6
Francisco Fernandez Novella <franferdez@gmail.com>
2019-03-24 14:26:36 +0100

    Hello world this is the initial commit

---
+++ frontend/src/components/app/app.js
@@ -0,0 +1,34 @@
+// @flow
+import React, { Component } from "react";
+import logo from "../../assets/images/logo.svg";
+import "./app.css";
+
+type Props = {
+  foo: number,
+  bar?: string
+};
+
+class App extends Component<Props> {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
