67ca5064dc50921181135c405c731f8be5a8af93
Francisco Fernandez Novella <franferdez@gmail.com>
2019-04-18 22:47:21 +0200

    remove old app initial js version

--- packages/backend/app/src/modules/user/index.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLModule } from "@graphql-modules/core";
-import * as typeDefs from "./schema.graphql";
-import resolvers from "./resolvers";
-
-export const User = new GraphQLModule({
-  typeDefs,
-  resolvers
-});
